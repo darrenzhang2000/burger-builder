fcde3429d83cab6b0b0cd826270c33ba49d86083
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-16 14:49:38 -0400

    added css

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  Aux from '../../hoc/auxillary'
+
+class BurgerBuilder extends React.Component{
+    render(){
+        return <Aux>
+            <div>burger pic</div>
+            <div>burger builder: modifies burger pic</div>
+        </Aux>
+    }
+}
+
+export default BurgerBuilder
