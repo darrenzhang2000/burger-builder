a4fd6ddd34edb5de86224c903f6b6d516bf99c05
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-16 16:56:12 -0400

    flattened array and added message to add ingredients if there are no ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,19 @@ import classes from "./Burger.css"
 import BurgerIngredient from "./Burgeringredient/Burgeringredient"
 
 const Burger = props => {
-    let ingredientsArr = Object.keys(props.ingredients).map(ingrKey => {
-        return [...Array(props.ingredients[ingrKey])].map((_, i) => {
-            return <BurgerIngredient type={ingrKey} key={ingrKey + i}/>
+    let ingredientsArr = Object.keys(props.ingredients)
+        .map(ingrKey => {
+            return [...Array(props.ingredients[ingrKey])].map((_, i) => {
+                return <BurgerIngredient type={ingrKey} key={ingrKey + i} />
+            })
         })
-    })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, [])
 
+        if(ingredientsArr.length === 0){
+            ingredientsArr = <p>Please start adding ingredients!</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
