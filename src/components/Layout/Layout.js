96e31935cae78ca7095dd6b4c92661a3c13fab9e
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-19 00:14:42 -0400

    fixed css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Aux from "../../hoc/auxillary"
 import classes from "./Layout.css"
+import Toolbar from "../Navigation/Toolbar/Toolbar"
 
 const Layout = props => (
     <Aux>   
-        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <Toolbar/>
         <main className={classes.Content}>{props.children}</main>
     </Aux>
 )
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,4 +1,5 @@
 import React from "react"
+import classes from "./Toolbar.css"
 
 const Toolbar = props => {
     return (
